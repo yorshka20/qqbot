f72552cf7cc60c5d276e967a424ffe941737a932
yorshka <mail@yors.hk>
2026-04-17 00:38:36 +0900

    chore: fix cors

--- src/services/staticServer/StaticServer.ts
+++ src/services/staticServer/StaticServer.ts
@@ -32,7 +32,7 @@ export type StaticServerInstance = {
 
 const CORS_HEADERS: Record<string, string> = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 };
 
